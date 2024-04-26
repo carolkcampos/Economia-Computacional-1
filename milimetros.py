9b6e161608cdc008bce86ea1f81e9f7b5af049f2
carolkcampos <167571947+carolkcampos@users.noreply.github.com>
2024-04-25 21:42:29 -0300

    milimetros.py
    This code converts values in meters to values in meters.

---
+++ milimetros.py
@@ -0,0 +1,20 @@
+"""
+Programa milimetros.py
+Descrição: Este programa converte valores em metros para milimetros.
+Versão: 0.0.1
+Data: 05/04/2024
+
+"""
+
+# Alocação de memória
+metros = 0
+milimetros = 0 
+
+# Entrada de dados
+metros = float(input("Metros"))
+
+# Processamento de dados
+milimetros = metros*1000
+
+# Saída de dados
+print(f"Este valor em milimetros é igual a {milimetros}")
